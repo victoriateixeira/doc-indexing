63c1f3fd97496920c38945d1a3045c16197068a9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-14 20:08:40 -0300

    test: implementa testes para a classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,39 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    regular_priority = {
+        "nome_do_arquivo": "arquivo_teste1.txt",
+        "qtd_linhas": 6,
+        "linhas_do_arquivo": [],
+    }
+    high_priority_1 = {
+        "nome_do_arquivo": "arquivo_teste2.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": [],
+    }
+    high_priority_2 = {
+        "nome_do_arquivo": "arquivo_teste3.txt",
+        "qtd_linhas": 4,
+        "linhas_do_arquivo": [],
+    }
+
+    priority_queue.enqueue(regular_priority)
+    priority_queue.enqueue(high_priority_1)
+    priority_queue.enqueue(high_priority_2)
+    assert len(priority_queue.regular_priority) == 1
+    assert len(priority_queue.high_priority) == 2
+
+    first_out = priority_queue.dequeue()
+    assert len(priority_queue.high_priority) == 1
+    assert len(priority_queue.regular_priority) == 1
+    assert first_out == high_priority_1
+
+    assert priority_queue.search(0) == high_priority_2
+    assert priority_queue.search(1) == regular_priority
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(50)
