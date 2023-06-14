56c8b518d5c18e9473e579352842d71ee312b87d
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-14 11:06:10 -0300

    feat: cria a classe Node

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,7 +3,6 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
 
     def __len__(self):
         """Aqui irá sua implementação"""
