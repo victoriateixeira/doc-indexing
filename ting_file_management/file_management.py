d6f0087fc24b98ebbd34a1c4df2c9e20e3b94aed
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-14 16:27:30 -0300

    feat: implementa a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    news_list = []
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido\n")
+
+        with open(path_file, "r") as file:
+            for line in file:
+                news_list.append(line.strip())
+        return news_list
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
