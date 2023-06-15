09375c48f356dfef11747949529a12fb22f4f533
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-14 21:32:30 -0300

    feat: implementa a função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_list = []
+    for file in instance._data:
+        lines = txt_importer(file)
+        occurencies = []
+        for index, line in enumerate(lines):
+            if word.lower() in line.lower():
+                occurencies.append({"linha": index + 1})
+
+        if len(occurencies) != 0:
+            words_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file,
+                    "ocorrencias": occurencies,
+                }
+            )
+
+    return words_list
 
 
 def search_by_word(word, instance):
