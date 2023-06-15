6851a48ff043e9da06f2b092b4e3112dca4f56ea
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-14 21:55:23 -0300

    feat: implementa a função search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,13 +2,25 @@ from ting_file_management.file_management import txt_importer
 
 
 def exists_word(word, instance):
+    words_list = search_by_word(word, instance)
+    if len(words_list) != 0:
+        for case in words_list:
+            for ocorrencia in case["ocorrencias"]:
+                ocorrencia.pop("conteudo")
+
+        return words_list
+    else:
+        return []
+
+
+def search_by_word(word, instance):
     words_list = []
     for file in instance._data:
         lines = txt_importer(file)
         occurencies = []
         for index, line in enumerate(lines):
             if word.lower() in line.lower():
-                occurencies.append({"linha": index + 1})
+                occurencies.append({"linha": index + 1, "conteudo": line})
 
         if len(occurencies) != 0:
             words_list.append(
@@ -20,7 +32,3 @@ def exists_word(word, instance):
             )
 
     return words_list
-
-
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
